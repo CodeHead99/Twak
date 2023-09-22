5e751f33c00e90dcaea6f8ecac39377c263956c0
CodeHead99 <akshitasg08@gmail.com>
2023-09-22 17:57:52 +0530

    chat display header and footer created

--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -1,11 +1,27 @@
 import React from "react";
 import Chats from "./Chats";
+import { Stack, Box } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import Conversation from "../../components/Conversation";
 
 const GeneralApp = () => {
+  const theme = useTheme();
   return (
-    <>
+    <Stack direction={"row"} sx={{ width: "100%" }}>
       <Chats />
-    </>
+      <Box
+        sx={{
+          height: "100%",
+          width: `calc(100vw - 415px)`,
+          backgroundColor:
+            theme.palette.mode === "light"
+              ? "#fff"
+              : theme.palette.background.default,
+        }}
+      >
+        <Conversation />
+      </Box>
+    </Stack>
   );
 };
 
