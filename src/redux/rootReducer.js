2b284a4d18e6a68e52b8d9fd43a4008a00284afb
Chakit Gupta <chakitg1@gmail.com>
2024-02-12 17:29:24 +0530

    added contact info sidebar

---
+++ src/redux/rootReducer.js
@@ -0,0 +1,19 @@
+import { combineReducers } from "redux";
+import storage from "redux-persist/lib/storage";
+import appReducer from "./slices/app";
+
+// slices
+
+const rootPersistConfig = {
+    key: 'root',
+    storage, 
+    keyPrefix: 'redux-',
+    //whitelist: [],
+    //blacklist: [],
+}
+
+const rootReducer = combineReducers({
+    app: appReducer,
+});
+
+export {rootPersistConfig, rootReducer};
