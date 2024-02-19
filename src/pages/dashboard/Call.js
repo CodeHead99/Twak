699e27b4ad74f4ea61b371f771cb022714d6a7db
CodeHead99 <akshitasg08@gmail.com>
2024-02-19 18:21:08 +0530

    call ui created Co-authored-by: chakitg www.chakitg@gmail.com Co-authored-by: CodeHead99  akshitasg08@gmail.com

--- src/pages/dashboard/Call.js
+++ src/pages/dashboard/Call.js
@@ -1,7 +1,105 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Box,
+  Stack,
+  Typography,
+  IconButton,
+  Link,
+  Divider,
+} from "@mui/material";
+import { MagnifyingGlass, Phone } from "phosphor-react";
+import { useTheme } from "@mui/material/styles";
+import { SimpleBarStyle } from "../../components/Scrollbar";
+
+import {
+  Search,
+  SearchIconWrapper,
+  StyledInputBase,
+} from "../../components/Search";
+import { CallLogElement } from "../../components/CallElement";
+import { CallLogs } from "../../data";
+import StartCall from "../../sections/main/StartCall";
 
 const Call = () => {
-  return <div>Call</div>;
+  const [openDialog, setOpenDialog] = useState(false);
+
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
+  };
+  const handleOpenDialog = () => {
+    setOpenDialog(true);
+  };
+  const theme = useTheme();
+  return (
+    <>
+      <Stack direction={"row"} sx={{ width: "100%" }}>
+        <Box
+          sx={{
+            overflowY: "scroll",
+
+            height: "100vh",
+            width: 320,
+            backgroundColor: (theme) =>
+              theme.palette.mode === "light"
+                ? "#F8FAFF"
+                : theme.palette.background.paper,
+
+            boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
+          }}
+        >
+          <Stack p={3} spacing={2} sx={{ maxHeight: "100vh" }}>
+            <Stack
+              alignItems={"center"}
+              justifyContent={"space-between"}
+              direction={"row"}
+            >
+              <Typography variant="h5">Call Log</Typography>
+            </Stack>
+            <Stack sx={{ width: "100%" }}>
+              <Search>
+                <SearchIconWrapper>
+                  <MagnifyingGlass color="#709CE6" />
+                </SearchIconWrapper>
+                <StyledInputBase
+                  placeholder="Search…"
+                  inputProps={{ "aria-label": "search" }}
+                />
+              </Search>
+            </Stack>
+            <Stack
+              justifyContent={"space-between"}
+              alignItems={"center"}
+              direction={"row"}
+            >
+              <Typography variant="subtitle2" sx={{}} component={Link}>
+                Start New Conversation
+              </Typography>
+              <IconButton onClick={handleOpenDialog}>
+                <Phone style={{ color: theme.palette.primary.main }} />
+              </IconButton>
+            </Stack>
+            <Divider />
+            <Stack sx={{ flexGrow: 1, overflowY: "scroll", height: "100%" }}>
+              <SimpleBarStyle timeout={500} clickOnTrack={false}>
+                <Stack spacing={2.4}>
+                  <Typography variant="subtitle2" sx={{ color: "#676667" }}>
+                    Pinned
+                  </Typography>
+                  {/* Call Logs */}
+                  {CallLogs.map((callLog) => (
+                    <CallLogElement {...callLog} />
+                  ))}
+                </Stack>
+              </SimpleBarStyle>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+      {openDialog && (
+        <StartCall open={openDialog} handleClose={handleCloseDialog} />
+      )}
+    </>
+  );
 };
 
 export default Call;
