464d1cca98739b852f22e1a28c1d27a27907a2d2
Chakit Gupta <chakitg1@gmail.com>
2024-02-23 08:33:14 +0530

    added comments and formatting

--- chat-server/app.js
+++ chat-server/app.js
@@ -1,17 +1,26 @@
-const express = require("express");
-const morgan = require("morgan");
+const express = require("express"); //web framework for node.js
+
+const morgan = require("morgan"); // http request logger middleware for node.js
+
 const rateLimit = require("express-rate-limit");
-const helmet = require("helmet");
+
+const helmet = require("helmet"); 
+
 const mongosanitize = require("express-mongo-sanitize");
+
 const bodyParser = require("body-parser");
+
 // const xss = require("xss");
+
 const cors = require("cors");
 
 const app = express();
+
 app.use(express.urlencoded({ extended: true }));
 // app.use(xss());
 
 app.use(mongosanitize());
+
 app.use(
   cors({
     origin: "*",
@@ -22,6 +31,7 @@ app.use(
 app.use(express.json({ limit: "10kb" }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(helmet());
 
 if (process.env.NODE_ENV !== "development") {
@@ -30,7 +40,7 @@ if (process.env.NODE_ENV !== "development") {
 
 const limiter = rateLimit({
   max: 3000,
-  windowMs: 60 * 60 * 1000,
+  windowMs: 60 * 60 * 1000, //in one hour
   message: "Too many requests, please try again later",
 });
 
--- chat-server/server.js
+++ chat-server/server.js
@@ -1,9 +1,13 @@
 const app = require("./app");
+
 const http = require("http");
 
 const server = http.createServer(app);
 
+// 3000, 5000
+
 const port = process.env.PORT || 8000;
+
 server.listen(port, () => {
   console.log(`listening on ${port}`);
 });
